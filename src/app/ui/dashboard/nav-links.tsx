d8612f19bdf8ec25d8c90f0d8ffeaf5cd151d192
kevinzheng117 <hzheng4@andrew.cmu.edu>
2024-06-25 16:58:05 +0800

    added navbar, in process of styling.

---
+++ src/app/ui/dashboard/nav-links.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import {
+  HomeIcon,
+  ListBulletIcon,
+  UserIcon
+} from '@heroicons/react/24/solid';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Dashboard', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Search',
+    href: '/dashboard/search',
+    icon: ListBulletIcon,
+  },
+  { name: 'Account', href: '/dashboard/account', icon: UserIcon },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-light-black p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'light-black': '#222222',
+      },
     },
   },
   plugins: [],
