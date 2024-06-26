8352c66d940873df65cc8a053220a5511bb5b5d3
zachkfan <zacharyf@andrew.cmu.edu>
2024-06-25 22:15:33 -0400

    made home_body, body_signup button, finished home_button, implemented custom color in themes to match our theme

---
+++ src/app/ui/home/home_button.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+import Link from 'next/link'
+
+const home_button  = () =>  {
+
+  return (
+    <Link href="/"><button className='text-black-gray bg-white cursor-pointer w-80 h-16 text-4xl text-center rounded-full font-black hover:scale-110 duration-100'>Start Today</button></Link>
+  )
+}
+
+export default home_button 
