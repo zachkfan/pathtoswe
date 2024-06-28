32f80f726e3a6ff88fc3b81d5012473c6f11f811
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-06-28 16:20:14 +0800

    added strict mode.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito, Playfair_Display } from "next/font/google";
 import "./ui/globals.css";
+import { StrictMode } from "react";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -24,11 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${playfairDisplay.variable} ${nunito.variable}`}
-    >
-      <body>{children}</body>
-    </html>
+    <StrictMode>
+      <html
+        lang="en"
+        className={`${playfairDisplay.variable} ${nunito.variable}`}
+      >
+        <body>{children}</body>
+      </html>
+    </StrictMode>
   );
 }
