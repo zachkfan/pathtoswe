1f9f42b97fbe21c5068c32a23b1195abde492558
cdrake0924 <chd5548@gmail.com>
2024-06-29 00:53:15 -0400

    update to the signup

--- src/app/signup/signup.tsx
+++ src/app/signup/signup.tsx
@@ -8,25 +8,25 @@ const sign_up_form = () => {
     }
     return (
     <div className="min-h-screen flex items-center justify-center bg-black-gray">
-        <img src="./black_gray_logo.png" className="absolute top-0 left-0 w-25 h-25 p-2" />
+        <img src="./black_gray_logo.png" className="absolute top-0 left-0 w-48 h-48 p-2" />
         <div className="flex-col justify-center w-2/5 py-8 px-14 bg-white text-black rounded-lg">
             <h1 className="text-3xl pb-5 font-semibold text-center">Sign Up</h1>
             <form>
                 <div className="mb-4">
                     <label htmlFor="name" className="block text-gray-700">Name</label>
-                    <input type="text" id="name" name="name" className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300" />
+                    <input type="text" id="name" name="name" className="bg-white mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="email" className="block text-gray-700">Email</label>
-                    <input type="email" id="email" name="email" className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300" />
+                    <input type="email" id="email" name="email" className="bg-white mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300" />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="password" className="block text-gray-700">Password</label>
-                    <input type="password" id="password" name="password" className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300" />
+                    <input type="password" id="password" name="password" className="bg-white mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300" />
                 </div>
                 <div className="mb-6">
                     <label htmlFor="confirmPassword" className="block text-gray-700">Confirm Password</label>
-                    <input type="password" id="confirmPassword" name="confirmPassword" className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300" />
+                    <input type="password" id="confirmPassword" name="confirmPassword" className="bg-white mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300" />
                 </div>
                 <button type="submit" className="w-full bg-black text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 h-12">Sign Up</button>
             </form>
@@ -38,7 +38,7 @@ const sign_up_form = () => {
             <div className="flex items-center justify-center">
                 <button className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md hover:bg-gray-100 h-12 w-full">
                     <img src="./google_logo.png" className="w-8 h-8 ml-1"></img>
-                    <span className="text-gray-700 font-semibold w-full mr-9">Sign in with Google</span>
+                    <span className="text-gray-700 font-semibold w-full mr-9">Sign up with Google</span>
                 </button>
             </div>
             <div className="flex flex-row items-center justify-center pt-4">
