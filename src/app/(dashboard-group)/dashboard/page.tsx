aa01145ec1e802c39a4bda7b6f829748a4521c1a
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-06-30 14:55:56 +0800

    finished sorting cards.

--- src/app/(dashboard-group)/dashboard/page.tsx
+++ src/app/(dashboard-group)/dashboard/page.tsx
@@ -1,10 +1,17 @@
 import SortingCard from "@/app/ui/dashboard/sorting-card";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 export default function Page() {
   return (
     <>
       <p>Dashboard Page</p>
-      <SortingCard />
+      <SortingCard
+        status="Closed"
+        icon={XMarkIcon}
+        applicationCount={2}
+        bgColor="#FFE2E5"
+        iconBgColor="#FA5A7D"
+      />
     </>
   );
 }
--- src/app/ui/dashboard/sorting-card.tsx
+++ src/app/ui/dashboard/sorting-card.tsx
@@ -1,18 +1,31 @@
-export default function SortingCards() {
+interface Props {
+  status: string;
+  icon: React.ComponentType;
+  applicationCount: number;
+  bgColor: string;
+  iconBgColor: string;
+}
+
+export default function SortingCards({
+  status,
+  icon: Icon,
+  applicationCount,
+  bgColor,
+  iconBgColor,
+}: Props) {
   return (
-    <div className="card bg-base-100 image-full w-96 shadow-xl">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+    <div className="card w-52 h-52" style={{ backgroundColor: bgColor }}>
+      <div className="card-body text-black gap-3">
+        <div
+          className="flex justify-center items-center w-12 h-12 rounded-full"
+          style={{ backgroundColor: iconBgColor }}
+        >
+          <div className="size-8 stroke-white text-white stroke-2">
+            <Icon />
+          </div>
         </div>
+        <h2 className="card-title font-extrabold text-2xl">{status}</h2>
+        <p>{applicationCount} applications</p>
       </div>
     </div>
   );
