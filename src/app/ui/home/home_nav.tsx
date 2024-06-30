8189468d7a346df32ba30abb13ad94fcaf82b05c
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-06-30 15:49:05 +0800

    deleted redundant code in home_nav.

--- src/app/ui/home/home_nav.tsx
+++ src/app/ui/home/home_nav.tsx
@@ -6,36 +6,6 @@ const home_nav = () => {
   return (
     <div className="navbar bg-transparent">
       <div className="flex-1">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-10 w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-concrete-gray rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <HomeNavLink
-              title={"Search Interships"}
-              href={"/search"}
-            ></HomeNavLink>
-            <HomeNavLink title={"Dashboard"} href={"/dashboard"}></HomeNavLink>
-            <HomeNavLink title={"Your Account"} href={"/account"}></HomeNavLink>
-            <HomeNavLink title={"Log In"} href={"/"}></HomeNavLink>
-          </ul>
-        </div>
         <Image
           className="hidden md:flex"
           src={"/black_logo.png"}
@@ -52,7 +22,7 @@ const home_nav = () => {
           ></HomeNavLink>
           <HomeNavLink title={"Dashboard"} href={"/dashboard"}></HomeNavLink>
           <HomeNavLink title={"Your Account"} href={"/account"}></HomeNavLink>
-          <HomeNavLink title={"Log In"} href={"/"}></HomeNavLink>
+          <HomeNavLink title={"Log In"} href={"/sign_in"}></HomeNavLink>
         </ul>
       </div>
     </div>
