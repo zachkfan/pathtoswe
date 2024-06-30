e702ded5a7b7d3c3bc9c1d1e21e2af5da1d69af8
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-06-30 13:50:00 +0800

    fixed layout for signin and signup.

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
-type LayoutProps = {
-    children: React.ReactNode;
-  };
-
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black-gray">
-        <img src="../black_gray_logo.png" className="absolute top-0 left-0 w-32 h-32 p-2" />
+      <img
+        src="../black_gray_logo.png"
+        className="absolute top-0 left-0 w-32 h-32 p-2"
+      />
       {children}
     </div>
   );
-};
-
-export default Layout;
+}
