26ba5f7b56d2f228f29ead445dc65cb272b0d63b
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-07-04 15:39:51 +0800

    implementred pagination for dashboard table.

--- src/app/ui/dashboard/table.tsx
+++ src/app/ui/dashboard/table.tsx
@@ -1,13 +1,37 @@
+"use client";
+
 import React from "react";
 import Row, { statusType } from "./dashboard_row";
 import { data } from "./data";
 import Card from "./dashboard_card";
+import FirstPageRoundedIcon from "@mui/icons-material/FirstPageRounded";
+import LastPageRoundedIcon from "@mui/icons-material/LastPageRounded";
+import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import CustomTablePagination from "@/app/ui/table_pagination";
 
 interface Props {
   search: string;
   cardView: boolean;
 }
 const Table = ({ search, cardView }: Props) => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (
+    _event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   if (cardView) {
     return (
       <div className="flex flex-row flex-wrap justify-evenly">
@@ -46,7 +70,10 @@ const Table = ({ search, cardView }: Props) => {
           </tr>
         </thead>
         <tbody className="text-black font-semibold">
-          {data
+          {(rowsPerPage > 0
+            ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : data
+          )
             .filter((item) => {
               return search.toLowerCase() == ""
                 ? item
@@ -66,6 +93,35 @@ const Table = ({ search, cardView }: Props) => {
               />
             ))}
         </tbody>
+        <tfoot>
+          <tr>
+            <CustomTablePagination
+              className="text-black text-sm"
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+              colSpan={7}
+              count={20} //TODO: switch with numbers of actual data
+              rowsPerPage={rowsPerPage}
+              page={page}
+              slotProps={{
+                select: {
+                  "aria-label": "rows per page",
+                },
+                actions: {
+                  showFirstButton: true,
+                  showLastButton: true,
+                  slots: {
+                    firstPageIcon: FirstPageRoundedIcon,
+                    lastPageIcon: LastPageRoundedIcon,
+                    nextPageIcon: ChevronRightRoundedIcon,
+                    backPageIcon: ChevronLeftRoundedIcon,
+                  },
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </tr>
+        </tfoot>
       </table>
     );
   }
