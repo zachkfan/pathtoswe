326b2107aeb1e721866caf38352b2a78095ac999
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-07-10 17:04:31 +0800

    restructured home page to establish client component boundary.

---
+++ src/app/ui/home/home.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Footer from "@/app/ui/footer";
+import ScrollButton from "@/app/ui/scroll_button";
+import { useRef, useEffect, useState } from "react";
+
+export default function HomeContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const footerRef = useRef<HTMLDivElement>(null);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    if (footerRef.current) {
+      setOffset(footerRef.current.offsetTop);
+    }
+  }, []);
+
+  return (
+    <>
+      <main className="font-sans">
+        {children}
+        <ScrollButton footerOffset={offset} />
+      </main>
+      <div ref={footerRef}>
+        <Footer />
+      </div>
+    </>
+  );
+}
