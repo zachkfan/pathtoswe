0f034f8fdaf23997390f0bddf9563f4824fa7594
zachkfan <zacharyf@andrew.cmu.edu>
2024-07-10 23:52:57 -0400

    new file in auth.config.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,58 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+// import prisma from '../../../../../lib/prisma'
+// import { session } from '@/lib/session'
+// import { NextAuthOptions } from 'next-auth'
+// import NextAuth from 'next-auth/next'
+// import GoogleProvider from 'next-auth/providers/google'
+
+// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
+// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
+
+// const authOption: NextAuthOptions = {
+//   session: {
+//     strategy: 'jwt',
+//   },
+//   providers: [
+//     GoogleProvider({
+//       clientId: GOOGLE_CLIENT_ID,
+//       clientSecret: GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   callbacks: {
+//     async signIn({ account, profile }) {
+//       if (!profile?.email) {
+//         throw new Error('No profile')
+//       }
+//       await prisma.users.upsert({
+//         where: {
+//           email: profile.email,
+//         },
+//         create: {
+//           email: profile.email,
+//           username: profile.name,
+//         },
+//         update: {
+//           username: profile.name,
+//         },
+//       })
+//       return true
+//     },
+//     session,
+//     async jwt({ token, user, account, profile }) {
+//       if (profile) {
+//         const user = await prisma.user.findUnique({
+//           where: {
+//             email: profile.email,
+//           },
+//         })
+//         if (!user) {
+//           throw new Error('No user found')
+//         }
+//         token.id = user.id
+//       }
+//       return token
+//     },
+//   },
+// }
+
+// const handler = NextAuth(authOption)
+// export { handler as GET, handler as POST }
---
+++ src/auth.config.ts
@@ -0,0 +1,20 @@
+import prisma from '../lib/prisma'
+// import { session } from '@/lib/session'
+import { NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth/next'
+import GoogleProvider from 'next-auth/providers/google'
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
+
+const authOption: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+  ]
+}
