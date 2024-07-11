650810302bcc4bb277a63d4a812d278e9359cf72
zachkfan <zacharyf@andrew.cmu.edu>
2024-07-11 06:01:04 -0400

    fix eslint?

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-import GitHub from "next-auth/providers/github"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google({
