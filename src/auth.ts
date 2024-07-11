60dec39bd17006b6767cc10eb856b41d12c19745
zachkfan <zacharyf@andrew.cmu.edu>
2024-07-11 05:46:35 -0400

    fixed googel auth somehow

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [Google({
+    clientId: process.env.AUTH_GOOGLE_ID,
+    clientSecret: process.env.AUTH_GOOGLE_SECRET,
+  })],
 })
