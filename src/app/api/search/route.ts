05c9b1a049c4c4ce3d0d235fc0ce53717b9d2775
zachkfan <zacharyf@andrew.cmu.edu>
2024-07-17 19:46:10 -0400

    fixed eslint?

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,12 +1,10 @@
-import client from "@/app/lib/prismadb";
+import prisma from "@/app/lib/prismadb";
 import { InternshipsType } from "@/app/lib/types";
 import { NextResponse } from "next/server";
 
 
-
-const prisma = client
-
 export async function GET(){
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     const internships = (await prisma.internships.findMany({})) as InternshipsType
     return NextResponse.json({internships})
 }
