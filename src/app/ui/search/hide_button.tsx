a108e2dcb56d3931f8c83962c4878ddf8232d19a
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-07-18 14:58:18 +0800

    added tooltips for save and hide buttons.

--- src/app/ui/search/hide_button.tsx
+++ src/app/ui/search/hide_button.tsx
@@ -3,9 +3,11 @@ import { EyeSlashIcon } from "@heroicons/react/24/outline";
 
 const HideButton = ({ rowHidden }: { rowHidden: () => void }) => {
   return (
-    <button onClick={() => setTimeout(rowHidden, 125)}>
-      <EyeSlashIcon className="w-6 text-black hidden hover:fill-black lg:block" />
-    </button>
+    <div className="tooltip" data-tip="Hide">
+      <button onClick={() => setTimeout(rowHidden, 125)}>
+        <EyeSlashIcon className="w-6 text-black hidden hover:fill-black lg:block" />
+      </button>
+    </div>
   );
 };
 
--- src/app/ui/search/save_button.tsx
+++ src/app/ui/search/save_button.tsx
@@ -3,11 +3,13 @@ import { BookmarkIcon } from "@heroicons/react/24/outline";
 
 const SaveButton = ({ rowHidden }: { rowHidden: () => void }) => {
   return (
-    <button onClick={() => setTimeout(rowHidden, 125)}>
-      {
-        <BookmarkIcon className=" w-6 text-black hidden hover:fill-black lg:block"></BookmarkIcon>
-      }
-    </button>
+    <div className="tooltip" data-tip="Save">
+      <button onClick={() => setTimeout(rowHidden, 125)}>
+        {
+          <BookmarkIcon className=" w-6 text-black hidden hover:fill-black lg:block"></BookmarkIcon>
+        }
+      </button>
+    </div>
   );
 };
 
