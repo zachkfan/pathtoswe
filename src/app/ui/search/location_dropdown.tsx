be1ce9e5ce9e8afc0866d39291117e4e7e66f905
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-07-18 15:05:03 +0800

    created location dropdown component.

---
+++ src/app/ui/search/location_dropdown.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function LocationDropdown(location: string) {
+  return (
+    <div className="dropdown dropdown-bottom">
+      <div tabIndex={0} role="button" className="btn m-1">
+        Click
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+      >
+        <li>
+          <a>Item 1</a>
+        </li>
+        <li>
+          <a>Item 2</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
