a2be8ee7cec6e4eb8b12820fca2a6d2653fad2f1
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-08-13 19:29:30 -0400

    in the process of creating ui for accunt page.

--- src/app/(dashboard-group)/account/page.tsx
+++ src/app/(dashboard-group)/account/page.tsx
@@ -8,14 +8,55 @@ export default async function Page() {
       <h1 className="text-4xl text-black-gray font-bold mb-12">
         Account Settings
       </h1>
-      <div className="flex w-full flex-col">
-        <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
-          content
-        </div>
-        <div className="divider divider-neutral"></div>
-        <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
-          content
-        </div>
+      <div className="flex flex-row justify-between items-end">
+        <form className="flex flex-col gap-4 w-[33vw]">
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text text-black-gray">Name</span>
+            </div>
+            <input
+              type="text"
+              placeholder={session?.user.name as string | undefined}
+              className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text text-black-gray">Email</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
+            />
+          </label>
+          <div className="mt-5 flex flex-row justify-between">
+            <input
+              k
+              type="submit"
+              value="Save Changes"
+              className="btn btn-info "
+            />
+            <input type="reset" value="Cancel" className="btn btn-outline" />
+          </div>
+        </form>
+        <button className="btn btn-outline btn-error">
+          Delete Account{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
