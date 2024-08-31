1c8b6f231dac2925b76709637ccd6694f63daec0
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-08-30 22:25:16 -0400

    fixed merge conflict.

--- src/app/ui/account/form.tsx
+++ src/app/ui/account/form.tsx
@@ -44,68 +44,92 @@ const form = ({ session }: { session: Session | null }) => {
   return (
     <>
       <Toaster toastOptions={{ className: "text-black font-semibold" }} />
-      <form className="flex flex-col gap-4 w-[33vw]" onSubmit={handleSubmit}>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-black-gray">Name</span>
+      <div className="p-6 flex flex-row justify-center items-end gap-4">
+        <div className="pl-24 flex flex-col items-center w-fit">
+          <h1 className="text-4xl text-black-gray font-bold mb-12 w-fit">
+            Account Settings
+          </h1>
+          <div className="flex flex-row gap-4 items-end w-fit">
+            <form
+              className="flex flex-col gap-4 w-[33vw]"
+              onSubmit={handleSubmit}
+            >
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text text-black-gray">Name</span>
+                </div>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder={session?.user.name as string | undefined}
+                  className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text text-black-gray">Email</span>
+                </div>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder={session?.user.email}
+                  className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text text-black-gray">
+                    Current Password
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  name="current_password"
+                  placeholder="Current Password"
+                  className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text text-black-gray">
+                    New Password
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="New Password"
+                  className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text text-black-gray">
+                    Confirm Password
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  name="password2"
+                  placeholder="Confirm Password"
+                  className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
+                />
+              </label>
+              <div className="mt-5 flex flex-row justify-between">
+                <input
+                  type="submit"
+                  value="Save Changes"
+                  className="btn btn-info "
+                />
+                <input
+                  type="reset"
+                  value="Cancel"
+                  className="btn btn-outline"
+                />
+              </div>
+            </form>
           </div>
-          <input
-            type="text"
-            name="username"
-            placeholder={session?.user.name as string | undefined}
-            className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-black-gray">Email</span>
-          </div>
-          <input
-            type="text"
-            name="email"
-            placeholder={session?.user.email}
-            className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-black-gray">Current Password</span>
-          </div>
-          <input
-            type="text"
-            name="current_password"
-            placeholder="Current Password"
-            className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-black-gray">New Password</span>
-          </div>
-          <input
-            type="text"
-            name="password"
-            placeholder="New Password"
-            className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-black-gray">Confirm Password</span>
-          </div>
-          <input
-            type="text"
-            name="password2"
-            placeholder="Confirm Password"
-            className="input input-bordered w-full bg-transparent border-2 border-gray-300 focus:border-gray-300 text-black-gray"
-          />
-        </label>
-        <div className="mt-5 flex flex-row justify-between">
-          <input type="submit" value="Save Changes" className="btn btn-info " />
-          <input type="reset" value="Cancel" className="btn btn-outline" />
         </div>
-      </form>
-      <div className="flex flex-row justify-between items-end">
         <DeleteButton id={session?.user.id}></DeleteButton>
       </div>
     </>
