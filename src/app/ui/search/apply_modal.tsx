4302f4baba2dbfe64c289561c17ab6aa7dcedfed
kevinzheng117 <zhenghaoyang141@gmail.com>
2024-11-20 00:47:49 -0500

    fixed applied modal styling.

--- src/app/ui/search/apply_modal.tsx
+++ src/app/ui/search/apply_modal.tsx
@@ -7,7 +7,7 @@ export default function ApplyModal({ company, role }: Props) {
   return (
     <div className="form-control flex flex-col flex-wrap items-center">
       <label className="cursor-pointer label">
-        <div className="label-text text-black w-full text-lg">
+        <div className="label-text text-black w-full text-lg text-center">
           Add{" "}
           <p className="font-bold inline">
             {company} - {role}
